9569d0920cb761ef40000b160ab6121517fc27d3
Mauricio Togneri <mauricio.togneri@gmail.com>
2026-09-13 22:29:05 +0200

    Revert "Improvements"
    This reverts commit e0d62ae9b27f53154bd4373106d6a0b5d1ebf963.

--- codex/scripts/notify.sh
+++ codex/scripts/notify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 paplay "$2" >/dev/null 2>&1 &
-flock -n "${XDG_RUNTIME_DIR:?}/notify-${PPID}.lock" zenity --info --text="$1" >/dev/null 2>&1 &
+zenity --info --text="$1" >/dev/null 2>&1 &
